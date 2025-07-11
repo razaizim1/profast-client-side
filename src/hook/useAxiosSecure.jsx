9dfd071a5d7a6b09834f59ad5b092fad48e81193
razaizim1 <zim.sarkar5956@gmail.com>
2025-07-11 07:59:00 +0600

    api secured

--- src/hook/useAxiosSecure.jsx
+++ src/hook/useAxiosSecure.jsx
@@ -1,11 +1,22 @@
 import axios from 'axios';
 import React from 'react';
-
+import useAuth from './useAuth';
 
 const axiosSecure = axios.create({
     baseURL: `http://localhost:3000`,
 });
 const useAxiosSecure = () => {
+    const { user } = useAuth();
+    const token = user?.accessToken;
+    // Add a request interceptor
+    axiosSecure.interceptors.request.use(function (config) {
+        // Do something before request is sent
+        config.headers.Authorization = `Bearer ${token}`;
+        return config;
+    }, function (error) {
+        // Do something with request error
+        return Promise.reject(error);
+    });
     return axiosSecure;
 };
 
